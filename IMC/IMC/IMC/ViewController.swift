2b64c04951f3c497f8f421ec82bc472bd4e7c228
Henrico Lazuroz <henrico.almeida@fcamara.com.br>
2020-05-23 00:00:46 -0300

    feat: add initial app IMC

---
+++ IMC/IMC/IMC/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  IMC
+//
+//  Created by Henrico Lazuroz on 22/05/20.
+//  Copyright © 2020 Henrico Lazuroz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tfWeigh:UITextField!
+    @IBOutlet weak var tfHeight: UITextField!
+    @IBOutlet weak var lbResult: UIView!
+    @IBOutlet weak var ivResult: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func calculate(_ sender: Any) {
+        
+    }
+}
