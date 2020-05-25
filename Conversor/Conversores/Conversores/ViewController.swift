b904c9a4e308df70bc04625a24923a1935fdd420
Henrico Lazuroz <henrico.almeida@fcamara.com.br>
2020-05-24 23:09:06 -0300

    feat: add initial project structure for Conversor app

---
+++ Conversor/Conversores/Conversores/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  Conversores
+//
+//  Created by Henrico Lazuroz on 24/05/20.
+//  Copyright © 2020 Henrico Lazuroz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tfValue: UITextField!
+    @IBOutlet weak var btUnit1: UIButton!
+    @IBOutlet weak var btUnit2: UIButton!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var lbResultUnit: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func showNext(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func convert(_ sender: UIButton) {
+        
+    }
+    
+
+}
+
