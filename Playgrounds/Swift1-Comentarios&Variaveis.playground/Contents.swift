ba2ba23837113f13ed9100748e351d90bb6d74a5
Henrico Lazuroz <henrico.almeida@fcamara.com.br>
2020-05-24 23:10:47 -0300

    feat: add folder Playgrounds for study Swift language

---
+++ Playgrounds/Swift1-Comentarios&Variaveis.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var str = "Hello, playground"
+
+//Comentário simples
+
+/*
+ * Comentário grande
+ * a
+ */
+
+var fatherAge = 56
+let year = 2020
+
+var fathersBirthYear = year - fatherAge
+print("The father birth in \(fathersBirthYear)")
---
+++ Playgrounds/Swift1-Tipos.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var str = "Hello, playground"
+
+// Tipos: Int, Float, Double, Char, String, Boolean
+
+var isFirstTime: Bool = true
+var likesFruits = true
+
+let text = """
+    Formatação de texto
+  Seguindo padrão
+Da String
+"""
+
+print(text)
