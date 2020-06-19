6622c0b08c6c751442bc30695a88519e568c36ee
Henrico <hicolazu22@hotmail.com>
2020-06-19 11:55:48 -0300

    feat: final heranca playground file

--- Playgrounds/Swift2/Swift2-Heranca.playground/Contents.swift
+++ Playgrounds/Swift2/Swift2-Heranca.playground/Contents.swift
@@ -25,9 +25,15 @@ class Dog: Animal {
 class Bird: Animal {
     let canFly: Bool
     
+    //Designated Initializer -> instância todas as propriedades do objeto
     init(color: String, gender: Character, breed: String, canFly: Bool) {
         self.canFly = canFly
         super.init(color: color, gender: gender, breed: breed)
+    }
+    
+    //Convenience Initializer
+    convenience init(color: String, breed: String) {
+        self.init(color: color, gender: "M", breed: breed, canFly: true)
     }
     
     func fly() {
@@ -43,5 +49,12 @@ class Bird: Animal {
 let billy = Dog(color: "Preto, Branco e Caramelo", gender: "M", breed: "Beagle")
 let zeCarioca = Bird(color: "Verde e amarelo", gender: "M", breed: "Papagaio", canFly: true)
 
+let maleBird = Bird(color: "Black", breed: "Sabiá")
+
+// 1) Designeted initializer deve chamar outro Designeted da sua classe pai
+// 2) Convenience initializer deve chamar outro initializer da mesma classe (self.init())
+// 3) Convenience initializer deve chamar, no final, um designeted initializer
+
 billy.emitSound()
 zeCarioca.emitSound()
+maleBird.emitSound()
