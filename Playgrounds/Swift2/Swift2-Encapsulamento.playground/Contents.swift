1d888db3ed6f6e3a4849b0b7aef1fdb8db632a18
Henrico <hicolazu22@hotmail.com>
2020-06-19 22:37:18 -0300

    feat: update encapsulamento file

--- Playgrounds/Swift2/Swift2-Encapsulamento.playground/Contents.swift
+++ Playgrounds/Swift2/Swift2-Encapsulamento.playground/Contents.swift
@@ -3,7 +3,7 @@ import UIKit
 class CreditCard {
     let number: String
     let limit: Double
-    var invoiceValue: Double = 0
+    private var invoiceValue: Double = 0
     
     let iof: Double = 6.38
     
@@ -22,8 +22,30 @@ class CreditCard {
             return true
         }
     }
+    
+    func getInvoiceValue() -> Double {
+        return self.invoiceValue
+    }
 }
 
 let creditCard = CreditCard(number: "1234-4445-2123-1235", limit: 5000)
 creditCard.makePurchase(off: 1500.00, isInternational: true)
-print(creditCard.invoiceValue)
+print(creditCard.getInvoiceValue())
+//print(creditCard.invoiceValue)
+
+class DebitCreditCard: CreditCard {
+    private var _balance: Double = 0
+    
+    var balance: Double {
+        return _balance
+    }
+    
+    func makeTransaction(of value: Double) {
+        _balance += value
+    }
+}
+
+let debitCreditCard = DebitCreditCard(number: "1234-1231-4121-4151", limit: 5000)
+debitCreditCard.makeTransaction(of: 500)
+debitCreditCard.makeTransaction(of: -100)
+print(debitCreditCard.balance)
